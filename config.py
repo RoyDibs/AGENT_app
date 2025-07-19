60a89b115eae2a01f1ccd825c40a8cb5fc342b8c
Bhaskar Roy <81694463+raycazy@users.noreply.github.com>
2025-07-20 03:16:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 # Document paths
 project_root = os.path.dirname(os.path.abspath(__file__))
 pdf_library = {
-    "chapter1": os.path.join(project_root, "chapter1.docx"),
-    "chapter2": os.path.join(project_root, "chapter2.docx"),
-    "chapter3": os.path.join(project_root, "chapter3.docx"),
-    "course information": os.path.join(project_root, "course syllabus.docx"),
+    "chapter1": os.path.join(project_root, "lecture.docx"),
+   # "chapter2": os.path.join(project_root, "chapter2.docx"),
+   # "chapter3": os.path.join(project_root, "chapter3.docx"),
+   # "course information": os.path.join(project_root, "course syllabus.docx"),
 }
 
 persist_directory = os.path.join(os.path.expanduser("~"), "aria_vectorstore")
